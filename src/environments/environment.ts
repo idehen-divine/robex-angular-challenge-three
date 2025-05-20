7fc832bd31036ca19b2b3ce58c2b26bfe38de86e
Idehen Divine <idehendivine16@gmail.com>
2025-05-20 01:25:32 +0100

    feat: inject environment variables into configuration for production build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  tinyUrlApiKey: 'tinyurl_api_key',
-  tinyUrlApiUrl: 'https://api.tinyurl.com/create',
-  localStorageKey: 'shortenedUrls'
+  tinyUrlApiKey: '',
+  tinyUrlApiUrl: '',
+  localStorageKey: ''
 };
