d300aa0a034c6960c3f43f9ef5da39fd4d02ca76
Idehen Divine <idehendivine16@gmail.com>
2025-05-20 01:32:50 +0100

    feat: replace TypeScript build script with JavaScript and inject environment variables into environment.ts

---
+++ vercel-build.js
@@ -0,0 +1,17 @@
+// scripts/replace-env.js
+
+const fs = require("fs");
+
+const envPath = "src/environments/environment.ts";
+
+const content = `
+export const environment = {
+  production: true,
+  tinyUrlApiKey: '${process.env.PUBLIC_TINYURL_API_KEY}',
+  tinyUrlApiUrl: '${process.env.PUBLIC_TINYURL_API_URL}',
+  localStorageKey: '${process.env.PUBLIC_LOCAL_STORAGE_KEY}',
+};
+`;
+
+fs.writeFileSync(envPath, content);
+console.log("✅ Environment variables injected into environment.ts");
