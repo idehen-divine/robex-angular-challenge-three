1ebe8d52c612d4aebe050d9314eb97afebf296fd
idehendivine16 <idehendivine16@gmail.com>
2025-05-23 15:06:20 +0000

    feat: configure Android development environment in IDX and update dependencies

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,31 +1,56 @@
-{pkgs}: {
+{ pkgs, ... }: {
   channel = "stable-24.05";
+  # extensions = [
+  #   "angular.ng-template"
+  #   "dbaeumer.vscode-eslint"
+  #   "sourcegraph.cody-ai"
+  #   "usernamehw.errorlens"
+  # ];
+  
   packages = [
-    pkgs.nodejs_2
-    pkgs.yarn 
+    pkgs.nodejs_18       # Compatible with Angular/Capacitor
+    pkgs.yarn
+    pkgs.jdk17           # Required for Android builds
+    pkgs.gradle          # Required for Android project
+    pkgs.unzip
+    pkgs.android-tools   # adb, emulator, sdkmanager, etc.
     pkgs.nano
   ];
-  idx.extensions = [
-    "angular.ng-template"
-    "dbaeumer.vscode-eslint"
-    "sourcegraph.cody-ai"
-    "usernamehw.errorlens"
-  ];
-  idx.previews = {
+  env = {
+    JAVA_HOME = "${pkgs.jdk17}/lib/openjdk";
+    ANDROID_SDK_ROOT = "/home/user/.androidsdkroot";
+  };
+  idx = {
     previews = {
-      web = {
-        command = [
-          "yarn"
-          "run"
-          "start"
-          "--"
-          "--port"
-          "$PORT"
-          "--host"
-          "0.0.0.0"
-          "--disable-host-check"
-        ];
-        manager = "web";
+      enable = true;
+      previews = {
+        # web = {
+        #   command = [
+        #     "yarn" "run" "start"
+        #     "--"
+        #     "--port" "$PORT"
+        #     "--host" "0.0.0.0"
+        #   ];
+        #   manager = "web";
+        # };
+        android = {
+          command = [
+            "yarn"
+            "run"
+            "android"
+          ];
+          manager = "android";
+        };
+      };
+    };
+
+    workspace = {
+      # Auto-setup project on first create (optional)
+      onCreate = {
+        install = ''
+          yarn
+          npx cap sync
+        '';
       };
     };
   };
