0ea0f1a474b0dee970545e5816ef48be5ffad27d
Idehen Divine <idehendivine16@gmail.com>
2025-05-24 09:11:08 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/components/shorten/shorten.component.ts
+++ src/app/components/shorten/shorten.component.ts
@@ -1,8 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ShortenerService } from '../../service/shortner/shortener.service';
 import { UrlInput } from '../../types/short/url-input';
+import { Network } from '@capacitor/network';
 
 @Component({
 	selector: 'app-shorten',
@@ -11,7 +12,7 @@ import { UrlInput } from '../../types/short/url-input';
 	standalone: true,
 	imports: [CommonModule, FormsModule],
 })
-export class ShortenComponent {
+export class ShortenComponent implements OnInit {
 	@ViewChild('urlInput') urlInput!: NgModel;
 
 	longUrl = '';
@@ -20,12 +21,29 @@ export class ShortenComponent {
 
 	shortenedLinks: UrlInput[] = [];
 	copiedUrl: string | null = null;
+	offline: boolean = false;
 
-	constructor(private shortenerService: ShortenerService) {
+	constructor(
+		private shortenerService: ShortenerService
+	) {
 		this.shortenedLinks = this.shortenerService.getTopShortenedUrls();
 	}
 
+	ngOnInit(): void {
+		Network.addListener('networkStatusChange', (status) => {
+			console.log('Network status changed:', status);
+			this.offline = !status.connected;
+		});
+	}
+
 	shortenUrl() {
+		if(this.offline)
+		{
+			this.error = 'You are offline. Please check your internet connection.';
+			this.loading = false;
+			return;
+		}
+		
 		this.error = null;
 		this.loading = true;
 
